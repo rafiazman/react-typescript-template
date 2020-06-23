090f13314b4018ecbba489fcb9f770a6822144ec
Matt Eden <mrmatteden@gmail.com>
2020-06-24 11:36:32 +1200

    Update default App with repo title + link

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React Template
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Matteas-Eden/react-template"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          View Source
         </a>
       </header>
     </div>
