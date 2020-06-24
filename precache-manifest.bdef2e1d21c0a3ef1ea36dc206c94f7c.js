d2b906dc79067d072a9d65926d24db7e511cfe1d
Matt Eden <mrmatteden@gmail.com>
2020-06-25 02:40:55 +1200

    Updates

---
+++ precache-manifest.bdef2e1d21c0a3ef1ea36dc206c94f7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "473517b05229126ed5a77cf861c4e22d",
+    "url": "/react-template/index.html"
+  },
+  {
+    "revision": "4f3012f66a4fb5247449",
+    "url": "/react-template/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b344a6cf2cb8c206fda0",
+    "url": "/react-template/static/js/2.7ab81765.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-template/static/js/2.7ab81765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f3012f66a4fb5247449",
+    "url": "/react-template/static/js/main.107ed6ee.chunk.js"
+  },
+  {
+    "revision": "22d71f0a45b553b62cb8",
+    "url": "/react-template/static/js/runtime-main.c9c040d8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-template/static/media/logo.5d5d9eef.svg"
+  }
+]);
