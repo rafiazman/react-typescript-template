30a6959ef8c51e4fe3e524b1bcf0a0a244dc833a
Matt Eden <mrmatteden@gmail.com>
2020-06-28 21:29:17 +1200

    Use scss instead of css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          React Template
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/Matteas-Eden/react-template"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          View Source
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>React Template</p>
+                <a
+                    className="App-link"
+                    href="https://github.com/Matteas-Eden/react-template"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    View Source
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
