a933b8db6862a358ee9b212ca1341b26c213438a
Rafi Azman <7603038+rafiazman@users.noreply.github.com>
2020-07-04 19:23:50 +1200

    Add TypeScript linting
    Executable by npm run lint

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,22 @@ import logo from './logo.svg';
 import './App.scss';
 
 const App = () => {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>React TypeScript Template</p>
-                <a
-                    className="App-link"
-                    href="https://github.com/rafiazman/react-typescript-template"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    View Source
-                </a>
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>React TypeScript Template</p>
+        <a
+          className="App-link"
+          href="https://github.com/rafiazman/react-typescript-template"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View Source
+        </a>
+      </header>
+    </div>
+  );
 };
 
 export default App;
